213e0df0ffeec99057da5cf040e2a4a309f3d54c
giorgidavitoshvili <119691539+giorgidavitoshvili@users.noreply.github.com>
2023-04-28 18:16:51 -0800

    homework2

---
+++ script.js
@@ -0,0 +1,37 @@
+let box = document.createElement("div");
+document.body.appendChild(box);
+
+box.style.marginTop = "70px";
+box.style.marginLeft = "550px";
+box.style.width = "600px";
+box.style.height = "665px";
+box.style.backgroundColor = "#b2acacf2";
+box.style.display = "flex";
+box.style.alignItems = "center";
+box.style.flexDirection = "column";
+
+let title = document.createElement("h1");
+title.innerText = "Blog Title";
+box.appendChild(title);
+
+let image = document.createElement("img");
+image.src = "gallery/1.jpg";
+box.appendChild(image);
+img.style.width = "200px";
+img.style.height = "150px";
+
+
+image.style.paddingTop = "15px";
+
+let text = document.createElement("p");
+text.innerText =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.";
+box.appendChild(text);
+
+text.style.paddingTop = "35px";
+text.style.maxWidth = "300px";
+text.style.fontSize = "17px";
+text.style.color = "black";
+
+
+
